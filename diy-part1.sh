22f2cd5042299539dbc6e917fe52b6b57f0e9968
cfj0316 <82389513+cfj0316@users.noreply.github.com>
2021-08-03 15:29:24 +0800

    Create diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -17,6 +17,20 @@
 #echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
 #git clone https://github.com/liuran001/openwrt-packages ./packages
 #echo 'src-git zerotier https://github.com/rufengsuixing/luci-app-zerotier' >>feeds.conf.default
-echo 'src-git liuran001_packages https://github.com/liuran001/openwrt-packages' >>feeds.conf.default
+#echo 'src-git liuran001_packages https://github.com/liuran001/openwrt-packages' >>feeds.conf.default
 git clone https://github.com/jerrykuku/luci-theme-argon ./package/mine/luci-theme-argon
 svn export --force https://github.com/coolsnowwolf/lede/trunk/package/lean/luci-app-zerotier ./package/mine/luci-app-zerotier
+svn export --force https://github.com/coolsnowwolf/lede/trunk/package/lean/luci-app-accesscontrol ./package/mine/luci-app-accesscontrol
+svn export --force https://github.com/coolsnowwolf/lede/trunk/package/lean/luci-app-adbyby-plus ./package/mine/luci-app-adbyby-plus 
+svn export --force https://github.com/coolsnowwolf/lede/trunk/package/lean/luci-app-airplay2 ./package/mine/luci-app-airplay2
+svn export --force https://github.com/coolsnowwolf/lede/trunk/package/lean/luci-app-arpbind ./package/mine/luci-app-arpbind
+svn export --force https://github.com/coolsnowwolf/lede/trunk/package/lean/luci-app-autoreboot ./package/mine/luci-app-autoreboot
+svn export --force https://github.com/coolsnowwolf/lede/trunk/package/lean/luci-app-filetransfer ./package/mine/luci-app-filetransfer
+svn export --force https://github.com/coolsnowwolf/lede/trunk/package/lean/luci-app-docker ./package/mine/luci-app-docker
+svn export --force https://github.com/coolsnowwolf/lede/trunk/package/lean/luci-app-frpc ./package/mine/luci-app-frpc
+svn export --force https://github.com/coolsnowwolf/lede/trunk/package/lean/luci-app-samba4 ./package/mine/luci-app-samba4
+svn export --force https://github.com/coolsnowwolf/lede/trunk/package/lean/luci-app-familycloud ./package/mine/luci-app-familycloud
+svn export --force https://github.com/coolsnowwolf/lede/trunk/package/lean/luci-app-turboacc ./package/mine/luci-app-turboacc
+svn export --force https://github.com/coolsnowwolf/lede/trunk/package/lean/luci-app-music-remote-center ./package/mine/luci-app-music-remote-center
+svn export --force https://github.com/coolsnowwolf/lede/trunk/package/lean/luci-app-netdata ./package/mine/luci-app-netdata
+svn export --force https://github.com/liuran001/openwrt-packages/trunk/luci-app-passwall ./package/mine/luci-app-passwall

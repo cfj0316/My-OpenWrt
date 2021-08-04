c88a5f1059bb410532c064ab6b0405f5beb6fb35
cfj0316 <82389513+cfj0316@users.noreply.github.com>
2021-08-04 08:54:39 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -14,33 +14,36 @@
 #sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
 #openwrt文件下下执行
 # Add a feed source
-#echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
-#git clone https://github.com/liuran001/openwrt-packages ./packages
+echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
 #echo 'src-git zerotier https://github.com/rufengsuixing/luci-app-zerotier' >>feeds.conf.default
 #echo 'src-git liuran001_packages https://github.com/liuran001/openwrt-packages' >>feeds.conf.default
-git clone https://github.com/jerrykuku/luci-theme-argon ./package/mine/luci-theme-argon
-svn export --force https://github.com/coolsnowwolf/lede/trunk/package/lean ./package/lean
-#svn export --force https://github.com/coolsnowwolf/lede/trunk/package/lean/luci-app-zerotier ./package/mine/luci-app-zerotier
-#svn export --force https://github.com/coolsnowwolf/lede/trunk/package/lean/luci-app-accesscontrol ./package/mine/luci-app-accesscontrol
-#svn export --force https://github.com/coolsnowwolf/lede/trunk/package/lean/luci-app-adbyby-plus ./package/mine/luci-app-adbyby-plus 
-#svn export --force https://github.com/coolsnowwolf/lede/trunk/package/lean/luci-app-airplay2 ./package/mine/luci-app-airplay2
-#svn export --force https://github.com/coolsnowwolf/lede/trunk/package/lean/luci-app-arpbind ./package/mine/luci-app-arpbind
-#svn export --force https://github.com/coolsnowwolf/lede/trunk/package/lean/luci-app-autoreboot ./package/mine/luci-app-autoreboot
-#svn export --force https://github.com/coolsnowwolf/lede/trunk/package/lean/luci-app-filetransfer ./package/mine/luci-app-filetransfer
-#svn export --force https://github.com/coolsnowwolf/lede/trunk/package/lean/luci-app-docker ./package/mine/luci-app-docker
-#svn export --force https://github.com/coolsnowwolf/lede/trunk/package/lean/luci-app-frpc ./package/mine/luci-app-frpc
-#svn export --force https://github.com/coolsnowwolf/lede/trunk/package/lean/luci-app-samba4 ./package/mine/luci-app-samba4
-#svn export --force https://github.com/coolsnowwolf/lede/trunk/package/lean/luci-app-familycloud ./package/mine/luci-app-familycloud
-#svn export --force https://github.com/coolsnowwolf/lede/trunk/package/lean/luci-app-turboacc ./package/mine/luci-app-turboacc
-#https://github.com/coolsnowwolf/lede/tree/master/package/lean/pdnsd-alt
-#https://github.com/coolsnowwolf/lede/tree/master/package/lean/dnsforwarder
-#https://github.com/coolsnowwolf/lede/tree/master/package/lean/dnsproxy
-#svn export --force https://github.com/coolsnowwolf/lede/trunk/package/lean/luci-app-music-remote-center ./package/mine/luci-app-music-remote-center
-#svn export --force https://github.com/coolsnowwolf/lede/trunk/package/lean/luci-app-netdata ./package/mine/luci-app-netdata
-#svn export --force https://github.com/liuran001/openwrt-packages/trunk/luci-app-passwall ./package/mine/luci-app-passwall
-#https://github.com/coolsnowwolf/lede/tree/master/package/lean/dns2socks
-#https://github.com/coolsnowwolf/lede/tree/master/package/lean/simple-obfs
-#https://github.com/coolsnowwolf/lede/tree/master/package/lean/trojan
-#https://github.com/coolsnowwolf/lede/tree/master/package/lean/dnsforwarder
-#https://github.com/coolsnowwolf/lede/tree/master/package/lean/csstidy
-#https://github.com/coolsnowwolf/lede/tree/master/package/lean/adbyby
+#git clone https://github.com/liuran001/openwrt-packages ./packages
+git clone https://github.com/jerrykuku/luci-theme-argon package/luci-theme-argon
+#svn export --force https://github.com/coolsnowwolf/lede/trunk/package/lean ./package/lean
+svn export --force https://github.com/coolsnowwolf/lede/trunk/package/lean/luci-app-zerotier ./package/luci-app-zerotier
+svn export --force https://github.com/coolsnowwolf/lede/trunk/package/lean/luci-app-accesscontrol ./package/luci-app-accesscontrol
+svn export --force https://github.com/coolsnowwolf/lede/trunk/package/lean/luci-app-adbyby-plus ./package/luci-app-adbyby-plus 
+svn export --force https://github.com/coolsnowwolf/lede/tree/master/package/lean/adbyby /package/adbyby
+svn export --force https://github.com/coolsnowwolf/lede/trunk/package/lean/luci-app-airplay2 ./package/luci-app-airplay2
+svn export --force https://github.com/coolsnowwolf/lede/trunk/package/lean/luci-app-arpbind ./package/luci-app-arpbind
+svn export --force https://github.com/coolsnowwolf/lede/trunk/package/lean/luci-app-autoreboot ./package/luci-app-autoreboot
+svn export --force https://github.com/coolsnowwolf/lede/trunk/package/lean/luci-app-filetransfer ./package/luci-app-filetransfer
+svn export --force https://github.com/coolsnowwolf/lede/trunk/package/lean/luci-app-docker ./package/luci-app-docker
+svn export --force https://github.com/coolsnowwolf/lede/tree/master/package/lean/luci-lib-docker ./package/luci-lib-docker
+svn export --force https://github.com/coolsnowwolf/lede/trunk/package/lean/luci-app-frpc ./package/luci-app-frpc
+svn export --force https://github.com/coolsnowwolf/lede/tree/master/package/lean/frp ./package/frp
+svn export --force https://github.com/coolsnowwolf/lede/trunk/package/lean/luci-app-samba4 ./package/luci-app-samba4
+svn export --force https://github.com/coolsnowwolf/lede/trunk/package/lean/luci-app-familycloud ./package/luci-app-familycloud
+svn export --force https://github.com/coolsnowwolf/lede/trunk/package/lean/luci-app-turboacc ./package/luci-app-turboacc
+svn export --force https://github.com/coolsnowwolf/lede/tree/master/package/lean/pdnsd-alt ./package/pdnsd-alt
+svn export --force https://github.com/coolsnowwolf/lede/tree/master/package/lean/dnsforwarder ./package/dnsforwarder
+svn export --force https://github.com/coolsnowwolf/lede/tree/master/package/lean/dnsproxy ./package/dnsproxy
+svn export --force https://github.com/coolsnowwolf/lede/trunk/package/lean/luci-app-music-remote-center ./package/luci-app-music-remote-center
+svn export --force https://github.com/coolsnowwolf/lede/trunk/package/lean/luci-app-netdata ./package/luci-app-netdata
+svn export --force https://github.com/liuran001/openwrt-packages/trunk/luci-app-passwall ./package/luci-app-passwall
+svn export --force https://github.com/coolsnowwolf/lede/tree/master/package/lean/dns2socks ./package/dns2socks
+svn export --force https://github.com/coolsnowwolf/lede/tree/master/package/lean/simple-obfs ./package/simple-obfs
+svn export --force https://github.com/coolsnowwolf/lede/tree/master/package/lean/trojan ./package/trojan
+svn export --force https://github.com/coolsnowwolf/lede/tree/master/package/lean/dnsforwarder ./package/dnsforwarder
+svn export --force https://github.com/coolsnowwolf/lede/tree/master/package/lean/csstidy ./package/csstidy
+

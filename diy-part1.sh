b4dbd0554983ac8ffa1899a692670f64e0de8aed
cfj0316 <82389513+cfj0316@users.noreply.github.com>
2021-08-04 16:19:24 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -16,7 +16,7 @@
 # Add a feed source
 #echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
 #echo 'src-git zerotier https://github.com/rufengsuixing/luci-app-zerotier' >>feeds.conf.default
-echo 'src-git liuran001_packages https://github.com/liuran001/openwrt-packages' >>feeds.conf.default
+#echo 'src-git liuran001_packages https://github.com/liuran001/openwrt-packages' >>feeds.conf.default
 #git clone https://github.com/liuran001/openwrt-packages ./packages
 #git clone https://github.com/jerrykuku/luci-theme-argon package/luci-theme-argon
 #svn export --force https://github.com/coolsnowwolf/lede/trunk/package/lean ./package/lean

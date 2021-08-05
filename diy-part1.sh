2d02baf2ccc4a9a4788a9e92fcebb203fd147e69
cfj0316 <82389513+cfj0316@users.noreply.github.com>
2021-08-05 10:25:43 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -19,7 +19,7 @@
 #echo 'src-git liuran001_packages https://github.com/liuran001/openwrt-packages' >>feeds.conf.default
 #git clone https://github.com/liuran001/openwrt-packages ./packages
 #git clone https://github.com/jerrykuku/luci-theme-argon package/luci-theme-argon
-#svn export --force https://github.com/coolsnowwolf/lede/trunk/package/lean ./package/lean
+svn export --force https://github.com/coolsnowwolf/lede/trunk/package/lean ./package/lean
 #svn export --force https://github.com/coolsnowwolf/lede/trunk/package/lean/luci-app-zerotier ./package/luci-app-zerotier
 #git clone https://github.com/rufengsuixing/luci-app-zerotier package/luci-app-zerotier
-echo 'src-git lede-lean https://github.com/cfj0316/lede/trunk/package/lean' >>feeds.conf.default
+#echo 'src-git lede-lean https://github.com/cfj0316/lede/trunk/package/lean' >>feeds.conf.default

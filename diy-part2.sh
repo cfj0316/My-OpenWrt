0379fedab625e8222c64586115ae8c92733eb9ed
cfj0316 <82389513+cfj0316@users.noreply.github.com>
2021-08-09 11:37:18 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -12,19 +12,3 @@
 
 # Modify default IP
 #sed -i 's/192.168.1.1/192.168.10.20/g' package/base-files/files/bin/config_generate
-
-if [ -f sdk.tar.xz ]; then
-	sed -i 's,$(STAGING_DIR_HOST)/bin/upx,upx,' package/feeds/custom/*/Makefile
-	mkdir sdk
-	tar -xJf sdk.tar.xz -C sdk
-	cp -rf sdk/*/staging_dir/* ./staging_dir/
-	rm -rf sdk.tar.xz sdk
-	rm -rf `find "staging_dir/host/" -maxdepth 2 -name 'libelf*'` || true
-	sed -i '/\(tools\|toolchain\)\/Makefile/d' Makefile
-	if [ -f /usr/bin/python ]; then
-		ln -sf /usr/bin/python staging_dir/host/bin/python
-	else
-		ln -sf /usr/bin/python3 staging_dir/host/bin/python
-	fi
-	ln -sf /usr/bin/python3 staging_dir/host/bin/python3
-fi

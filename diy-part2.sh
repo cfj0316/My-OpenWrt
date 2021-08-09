ee20ad0c5c6e7bb7a5ccaa051b45cb50bbfe63bd
cfj0316 <82389513+cfj0316@users.noreply.github.com>
2021-08-09 09:23:15 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -12,49 +12,6 @@
 
 # Modify default IP
 #sed -i 's/192.168.1.1/192.168.10.20/g' package/base-files/files/bin/config_generate
-#!/bin/bash
-#=================================================
-#rm -Rf feeds/custom/diy
-#./scripts/feeds update luci packages custom
-#./scripts/feeds install -a -p custom
-#./scripts/feeds install -a
-#sed -i 's/Os/O2/g' include/target.mk
-rm -rf target/linux package/kernel
-svn export https://github.com/openwrt/openwrt/trunk/target/linux target/linux
-curl -L https://raw.githubusercontent.com/openwrt/openwrt/master/include/kernel-version.mk>include/kernel-version.mk
-svn export https://github.com/openwrt/openwrt/trunk/package/kernel package/kernel
-curl -L https://raw.githubusercontent.com/openwrt/openwrt/master/include/kernel-defaults.mk>include/kernel-defaults.mk
-rm -Rf tools/upx && svn export https://github.com/coolsnowwolf/lede/trunk/tools/upx tools/upx
-rm -Rf tools/ucl && svn export https://github.com/coolsnowwolf/lede/trunk/tools/ucl tools/ucl
-sed -i 's?zstd$?zstd ucl upx\n$(curdir)/upx/compile := $(curdir)/ucl/compile?g' tools/Makefile
-sed -i 's/ libelf//' tools/Makefile
-svn co https://github.com/immortalwrt/immortalwrt/branches/master/target/linux/generic/hack-5.4 target/linux/generic/hack-5.4
-svn co https://github.com/immortalwrt/immortalwrt/branches/master/target/linux/generic/hack-5.10 target/linux/generic/hack-5.10
-rm -rf target/linux/generic/hack-5.10/220-gc_sections.patch
-svn export --force https://github.com/immortalwrt/immortalwrt/branches/openwrt-21.02/package/network/services/ppp package/network/services/ppp
-svn export --force https://github.com/openwrt/openwrt/trunk/package/libs/libnfnetlink package/libs/libnfnetlink
-svn export --force https://github.com/immortalwrt/immortalwrt/branches/master/package/network/services/dnsmasq package/network/services/dnsmasq
-sed -i 's/$(TARGET_DIR)) install/$(TARGET_DIR)) install --force-overwrite/' package/Makefile
-sed -i '$a /etc/sysupgrade.conf' package/base-files/files/lib/upgrade/keep.d/base-files-essential
-sed -i '$a /etc/amule' package/base-files/files/lib/upgrade/keep.d/base-files-essential
-sed -i '$a /etc/acme' package/base-files/files/lib/upgrade/keep.d/base-files-essential
-sed -i '$a /etc/bench.log' package/base-files/files/lib/upgrade/keep.d/base-files-essential
-sed -i '/\/etc\/profile/d' package/base-files/files/lib/upgrade/keep.d/base-files-essential
-sed -i '/^\/etc\/profile/d' package/base-files/Makefile
-# find target/linux/x86 -name "config*" -exec bash -c 'cat kernel.conf >> "{}"' \;
-#sed -i '$a CONFIG_ACPI=y\nCONFIG_X86_ACPI_CPUFREQ=y\nCONFIG_NR_CPUS=128\nCONFIG_FAT_DEFAULT_IOCHARSET="utf8"\nCONFIG_CRYPTO_CHACHA20_NEON=y\n \
-#CONFIG_CRYPTO_CHACHA20POLY1305=y\nCONFIG_BINFMT_MISC=y' `find target/linux -path "target/linux/*/config-*"`
-sed -i 's/max_requests 3/max_requests 20/g' package/network/services/uhttpd/files/uhttpd.config
-#rm -rf ./feeds/packages/lang/{golang,node}
-#svn export https://github.com/immortalwrt/packages/trunk/lang/golang feeds/packages/lang/golang
-#svn export https://github.com/immortalwrt/packages/trunk/lang/node feeds/packages/lang/node
-#wget -P package/network/config/firewall/patches/ https://github.com/coolsnowwolf/lede/raw/master/package/network/config/firewall/patches/fullconenat.patch
-#sed -i "s/+nginx\( \|$\)/+nginx-ssl\1/g"  package/*/*/*/Makefile
-#sed -i 's/+python\( \|$\)/+python3/g' package/*/*/*/Makefile
-#sed -i 's?../../lang?$(TOPDIR)/feeds/packages/lang?g' package/feeds/custom/*/Makefile
-#sed -i 's/PKG_BUILD_DIR:=/PKG_BUILD_DIR?=/g' feeds/luci/luci.mk
-#sed -i 's?admin/status/channel_analysis??' package/feeds/luci/luci-mod-status/root/usr/share/luci/menu.d/luci-mod-status.json
-#sed -i "s/askfirst/respawn/g" `find package target -name inittab`
 
 if [ -f sdk.tar.xz ]; then
 	sed -i 's,$(STAGING_DIR_HOST)/bin/upx,upx,' package/feeds/custom/*/Makefile

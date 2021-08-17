883ff6a99e85da5b514adbf4cf80bc63de6f3844
cfj0316 <82389513+cfj0316@users.noreply.github.com>
2021-08-17 11:41:34 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -12,24 +12,3 @@
 
 # Modify default IP
 sed -i 's/192.168.1.1/192.168.10.20/g' package/base-files/files/bin/config_generate
-rm -rf package/feeds/custom/brook
-rm -rf package/feeds/custom/chinadns-ng
-rm -rf package/feeds/custom/dns2socks
-rm -rf package/feeds/custom/ipt2socks
-rm -rf package/feeds/custom/kcptun
-rm -rf package/feeds/custom/luci-app-passwall
-rm -rf package/feeds/custom/microsocks
-rm -rf package/feeds/custom/naiveproxy
-rm -rf package/feeds/custom/pdnsd-alt
-rm -rf package/feeds/custom/shadowsocks-rust
-rm -rf package/feeds/custom/shadowsocksr-libev
-rm -rf package/feeds/custom/simple-obfs
-rm -rf package/feeds/custom/ssocks
-rm -rf package/feeds/custom/tcping
-rm -rf package/feeds/custom/trojan-go
-rm -rf package/feeds/custom/trojan-plus
-rm -rf package/feeds/custom/trojan
-rm -rf package/feeds/custom/v2ray-core
-rm -rf package/feeds/custom/v2ray-plugin
-rm -rf package/feeds/custom/xray-core
-rm -rf package/feeds/custom/xray-plugin
